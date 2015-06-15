8e52863c53c37a8749d1792c4d6a687e6dea51f3
Robin <roroettg@gmail.com>
2015-06-15 13:58:39 +0200

    TestData generalized

--- src/main/java/InputHandler.java
+++ src/main/java/InputHandler.java
@@ -1,5 +1,42 @@
+
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.SplittableRandom;
+
 /**
  * Created by robin on 15.06.15.
  */
 public class InputHandler {
+
+    public static void main(String[] args) {
+        JSONParser parser = new JSONParser();
+
+        try {
+            JSONObject jsonObject = (JSONObject) parser.parse(new FileReader("/home/robin/Mustererkennung/Measurements/LernDaten/02/gehen.json"));
+
+            Characteristic c = new Characteristic(jsonObject);
+            for(String key: c.characteristics.keySet()) {
+                HashMap<String, Double>  data = c.characteristics.get(key);
+                for(String datakey : data.keySet()) {
+                    System.out.println("Sensor:" + key);
+                    System.out.println("Datakey" + datakey);
+                    System.out.println("Value" + data.get(datakey));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
 }
